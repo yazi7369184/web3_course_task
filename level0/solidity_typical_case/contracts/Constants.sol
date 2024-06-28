99b57543f9fe8763e37c38e3409af1f35c3daa80
binfeng.yang <binfeng.yang@eoitek.com>
2024-06-28 17:41:38 +0800

    Solidity 经典用例

---
+++ level0/solidity_typical_case/contracts/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    address public constant MY_ADDRESS =
+    0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
---
+++ level0/solidity_typical_case/contracts/Variables.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint256 public num = 123;
+
+    function doSomething() public {
+        // Local variables are not saved to the blockchain.
+        uint256 i = 456;
+
+        // Here are some global variables
+        uint256 timestamp = block.timestamp; // Current block timestamp
+        address sender = msg.sender; // address of the caller
+    }
+}
